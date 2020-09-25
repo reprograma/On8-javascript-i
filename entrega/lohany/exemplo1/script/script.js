3dda16e01c5d9db8bdaa04c15d56eba897fda551
lohanyformiga <lohany.formiga@hotmail.com>
2020-09-25 15:16:03 -0300

    atualizando o fork

---
+++ entrega/lohany/exemplo1/script/script.js
@@ -0,0 +1,24 @@
+//Capturar o valor inserido no input quando acionar o botão
+//Retornar em um pop-up uma mensagem de sucesso para o usuário quando acionar o botão
+//Adicionar um novo campo para a mensagem de sucesso quando acionar o botão
+//Adicionar uma mensagem de sucesso personalizada com o email informado
+//Alterar a cor do texto caso o email tenha sido cadastrado com sucesso
+//Validar se os campos estão vazios ou não
+
+let input = document.querySelector('#newsInputEmail');
+let button = document.querySelector('.button');
+let mensagem = document.querySelector('.mensagem');
+
+button.addEventListener('click', function (event) {
+  event.preventDefault();
+  let valorDoInput = input.value;
+
+  if (valorDoInput == '') {
+    mensagem.textContent = 'Por favor, preencha o campo de email';
+    mensagem.style.color = 'red';
+  } else {
+    mensagem.textContent = `Seu email: ${valorDoInput} foi cadastrado com sucesso`;
+    mensagem.style.color = 'green';
+    input.value = '';
+  }
+}); 
---
+++ entrega/lohany/exercicioParaCasa/1/script/script.js
@@ -0,0 +1,72 @@
+// Calculadora de média de provas
+// Você irá construir uma aplicação web com os seguintes requisitos:
+
+// Calcula o valor médio entre duas notas e informa ao aluno seu resultado quando o botão é acionado
+// Resultados: APROVADO > 70%
+// Resultados: REPROVADO < 70%
+
+// Exibir na mensagem a média atual do aluno.
+// Ex: Parabéns! Você foi aprovado com a média X
+// Ex: Infelizmente, você não atingiu a nota mínima para aprovação. Sua média é X
+
+// Validar campos de input para não aceitar valores vazios e formatos inválidos
+
+// ATENÇÃO - LEMBRE-SE QUE PARA TRANSFORMAR STRING(texto) EM NÚMERO É NECESSÁRIO UTILIZAR FUNÇÕES, PEQUISE parseInt ou parseFloat
+
+const botaoCalcular = document.querySelector('#button');
+const mensagem = document. querySelector('.media-titulo');
+const botaoApagar = document.querySelector('.clear');
+
+function calculaMedia(n1, n2) {
+   return Math.round( ( n1+n2) / 2);
+}
+
+
+botaoCalcular.addEventListener('click', function (event) {
+  event.preventDefault();
+  const nota1 = parseFloat(document.querySelector('.nota1').value);
+  const nota2 = parseFloat(document.querySelector('.nota2').value);
+  let mediaFinal = calculaMedia(nota1, nota2);
+
+  if (mediaFinal >= 7 && mediaFinal <= 10) {
+  mensagem.textContent = `Parabéns! Você foi aprovado com a média é ${mediaFinal}`;
+    mensagem.classList.add('aprovacao');
+    mensagem.classList.remove('reprovacao');
+    mensagem.classList.remove('mensagem-de-erro');
+    
+    }
+   if(mediaFinal < 7 && mediaFinal >= 0) {
+      mensagem.textContent = `Que pena, sua nota foi baixa. Sua nota é ${mediaFinal}`; 
+
+      mensagem.classList.add('reprovacao');
+      mensagem.classList.remove('aprovacao');
+      mensagem.classList.remove('mensagem-de-erro');
+
+    }
+
+     if (isNaN(nota1) || isNaN(nota2)) {
+        mensagem.textContent ='Preencha corretamente os campos, com valores númericos entre 0 e 10';
+        mensagem.classList.add('mensagem-de-erro');
+        mensagem.classList.remove('aprovacao');
+        mensagem.classList.remove('aprovacao');
+
+        }
+
+        if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 ){
+          mensagem.textContent ='Preencha com valores entre 0 e 10';
+          mensagem.classList.add('mensagem-de-erro');
+    }
+});
+
+botaoApagar.eddEventListenner('click', function(event){
+event.preventDefault();
+
+document.querySelector('.nota1').value = '';
+document.querySelector('.nota2').value = '';
+
+mensagem.classList.remove('reprovacao')
+mensagem.classList.remove('aprovacao')
+mensagem.classList.remove('mensagem-de-erro')
+mensagem.textContent ='Sua média é:';
+
+});
